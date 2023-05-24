1d4063b28805d90d22d788e73938dc4ec8ecf69b
torfstack <torfstack@gmail.com>
2023-05-25 00:38:16 +0200

    switch from jsdoc to typescript

---
+++ src/routes/secrets/+page.server.ts
@@ -0,0 +1,17 @@
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({params}) => {
+    const resp = await getSecretsFromServer()
+    const text = await resp.text()
+    return {
+        text: text
+    };
+}) satisfies PageServerLoad
+
+
+async function getSecretsFromServer() {
+    return fetch("http://localhost:8080/secret")
+}
+
+
+
