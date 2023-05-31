b2cc69b62f4b586fe0b0bc8fbf5f29a95261eb0c
torfstack <torfstack@gmail.com>
2023-06-01 00:17:18 +0200

    added: bootstrap and login form with google signup and email/pw registration/login form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,22 +21,27 @@
 <h1>KayVault</h1>
 <p>Register or Login to manage your secrets</p>
 
-<div class="login row">
-    <div class="column">
-        <input bind:value={email} type="text" placeholder="Email" name="username">
-    </div>
-    <div class="column">
-        <input bind:value={password} type="text" placeholder="Password" name="password">
-    </div>
-    <div class="column">
-        <button on:click={registerNewUser}>Register</button>
-    </div>
-    <div class="column">
-        <button>Login</button>
-    </div>
-    <div class="column">
-        <button>Login with Google</button>
-    </div>
+<div class="container">
+    <form>
+        <div class="mb-3">
+            <a class="btn btn-light text-uppercase btn-outline" href="#"><img src="https://img.icons8.com/color/16/000000/google-logo.png">Signup Using Google</a>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input bind:value={email} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            <div id="emailHelp" class="form-text">We use Firebase Authentication to store user login information.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input bind:value={password} type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+        <button type="submit" class="btn btn-secondary" on:click={registerNewUser}>Register</button>
+    </form>
 </div>
 
 <style>
