2bb3b33b38f342cb6a191a645242f51122b6c689
torfstack <torfstack@gmail.com>
2023-06-04 23:53:53 +0200

    added: modal to create new secret

--- src/routes/secrets.svelte
+++ src/routes/secrets.svelte
@@ -4,9 +4,13 @@
 
     export let currentUser: UserCredential | null
 
-    let value: string = "";
+    let filterValue: string = "";
+    let inputKey: string = "";
+    let inputValue: string = "";
+    let inputUrl: string = "";
+
     let secrets: string[] = []
-    $: shown = secrets.filter((s: string) => s.indexOf(value) != -1)
+    $: shown = secrets.filter((s: string) => s.indexOf(filterValue) != -1)
 
     function getSecretsFromServer() {
         let user = currentUser as UserCredential
@@ -23,7 +27,7 @@
         });
     }
 
-    function uploadSecret(secret: string) {
+    function uploadSecret() {
         let user = currentUser as UserCredential
         return user.user.getIdToken().then(async token => {
             console.log(token)
@@ -36,19 +40,13 @@
                     "Authorization": "Bearer" + token
                 },
                 body: JSON.stringify({
-                    value: secret,
+                    value: inputValue,
                     key: "something"
                 })
             })
             .then(resp => resp.json())
             .then(body => secrets = body)
         })
-    }
-
-    function handleKeydown(event: KeyboardEvent) {
-        if (event.code == "Enter") {
-            uploadSecret(value)
-        }
     }
 
     function logout() {
@@ -61,7 +59,7 @@
 
 <style>
     input {
-        width: 80%;
+        width: 100%;
         padding: 12px 20px;
         margin: 8px 0;
         font-weight: 300;
@@ -72,22 +70,65 @@
     .secrets {
         text-align: center;
     }
+    .create {
+        text-align: center;
+        margin: auto;
+    }
+    .button-create {
+        width: 100%;
+    }
 </style>
 
 <html lang="en">
-    <div class="p-3">
-        <h1 class="display-3">Manage your secrets</h1>
-        <small class="text-body-secondary">Start typing to filter secrets and add new ones, or <a on:click={logout}>logout</a></small>
-
-        <div class="container-fluid">
-            <div class="input">
-                <input bind:value type="text" placeholder="Add/Search Secrets" name="New Secret" on:keypress={handleKeydown}>
+    <body>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" >
+            <div class="modal-dialog model-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">New secret</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="inputKey" class="form-label">Name</label>
+                            <input bind:value={inputKey} type="text" class="form-control" id="inputKey">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputValue" class="form-label">Secret</label>
+                            <input bind:value={inputValue} type="text" class="form-control" id="inputValue">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputUrl" class="form-label">URL</label>
+                            <input bind:value={inputUrl} type="text" class="form-control" id="inputUrl">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" on:click={uploadSecret}>Save changes</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="secrets">
-            {#each shown as secret}
-                <p>{secret}</p>
-            {/each}
+
+        <div class="p-3">
+            <h1 class="display-3">Manage your secrets</h1>
+            <small class="text-body-secondary">Start typing to filter secrets and add new ones, or <a on:click={logout}>logout</a></small>
+
+            <div class="container text-center">
+                <div class="row">
+                    <div class="input col-10">
+                        <input bind:value={filterValue} type="text" placeholder="Add/Search Secrets" name="New Secret">
+                    </div>
+                    <div class="col create">
+                        <button type="button" class="button-create btn btn-primary text-uppercase btn-outline" data-bs-toggle="modal" data-bs-target="#exampleModal">Create</button>
+                    </div>
+                </div>
+            </div>
+            <div class="secrets">
+                {#each shown as secret}
+                    <p>{secret}</p>
+                {/each}
+            </div>
         </div>
-    </div>
+    </body>
 </html>
